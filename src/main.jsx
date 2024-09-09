c18e0e9bfd4c813c2b100976aa4ee0601074a263
“Flavia <flaviferri90@gmail.com>
2024-09-09 11:43:04 +0200

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { router } from "./router";
 import "./index.css";
-import {RouterProvider} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-  <RouterProvider router={router} />
+<RouterProvider router={router}/>
   //</React.StrictMode>,
 );
